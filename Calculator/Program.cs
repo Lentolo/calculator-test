6985e38fce6067634639ae4ff42794aa1151ef02
Martini Arialdo <arialdo.martini@tcpos.com>
2018-05-28 16:05:27 +0200

    A Calculator supporting sums and input from consoled and files

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      if (args[0] == "interactive")
+      {
+        Console.WriteLine("First argument: ");
+        var readLine = Console.ReadLine();
+
+        Console.WriteLine("Second Argument: ");
+        var secondArgoument = Console.ReadLine();
+
+        Console.WriteLine("Result" + (int.Parse(readLine) + int.Parse(secondArgoument)));
+      }
+      else
+      {
+        var readAllText = System.IO.File.ReadAllText(args[0]);
+        var strings = readAllText.Split("\r\n".ToCharArray());
+
+        var firstArgument = strings[0];
+
+        var secondArgoument = strings[1];
+
+        Console.WriteLine("Result" + (int.Parse(firstArgument) + int.Parse(secondArgoument)));
+      }
+    }
+  }
+}
+
